892e49dc42152b3b4a766e5efb0a0f891fcaaf8a
xiecf <xcf_love@live.cn>
2021-08-20 04:08:19 +0000

    添加字段注释

--- flyway/sql/V1__dml_create_user.sql
+++ flyway/sql/V1__dml_create_user.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS sys_user (
-id int auto_increment primary key,
-full_name VARCHAR(16),
-age int
+id int auto_increment primary key comment '主键Id',
+full_name VARCHAR(16) comment '全称',
+age int comment '年龄'
 );
